c5fad254611d32f1d13a9eb99e729b07e6b0a3a3
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-07-21 23:16:09 +0800

    Added user login UI

---
+++ pages/userLogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEUST CLINIC</title>
+    <link rel="stylesheet" href="../css/user.css">
+</head>
+<body>
+    
+    
+    <div class="userLogin-container">
+        <div class="form-container">
+            <form action="" class="login-form">
+                <h4>USER LOGIN</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <button>Log-In</button>
+                <a href="/register">REGISTER</a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- routes.php
+++ routes.php
@@ -2,42 +2,13 @@
 
 require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
 
-// ##################################################
-// ##################################################
-// ##################################################
-
-// Static GET
-// In the URL -> http://localhost
-// The output -> Index
-get('/', 'index.php');
-
-// Dynamic GET. Example with 1 variable
-// The $id will be available in user.php
-get('/user/$id', 'user.php');
-
-// Dynamic GET. Example with 2 variables
-// The $name will be available in user.php
-// The $last_name will be available in user.php
-get('/user/$name/$last_name', 'user.php');
-
-// Dynamic GET. Example with 2 variables with static
-// In the URL -> http://localhost/product/shoes/color/blue
-// The $type will be available in product.php
-// The $color will be available in product.php
-get('/product/$type/color/:color', 'product.php');
-
-// Dynamic GET. Example with 1 variable and 1 query string
-// In the URL -> http://localhost/item/car?price=10
-// The $name will be available in items.php which is inside the views folder
-get('/item/$name', 'views/items.php');
+//ADMIN ROUTES
 
 
-// ##################################################
-// ##################################################
-// ##################################################
-// any can be used for GETs or POSTs
+//USER ROUTES
+get('/', 'pages/userLogin.php');
 
-// For GET or POST
-// The 404.php which is inside the views folder will be called
-// The 404.php has access to $_GET and $_POST
-any('/404','views/404.php');
+get('/register', 'pages/userRegister.php');
+
+// ERROR ROUTES
+any('/404','pages/404.php');
