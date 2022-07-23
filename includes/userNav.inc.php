f208f199b9aab1628866128c56d14381526a6a41
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-07-24 05:30:10 +0800

    Tested some bugs, refactored some code in navigation and added mock ui for user dashboard

--- includes/userNav.inc.php
+++ includes/userNav.inc.php
@@ -1,31 +1,31 @@
 <nav class="navbar">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="#dashboard" class="nav-link">
                 <i class="fa-solid fa-square-poll-vertical"></i>
                 <span class="link-text">Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="#user-information" class="nav-link">
                 <i class="fa-solid fa-user"></i>
                 <span class="link-text">User Information</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="#visit-records" class="nav-link">
                 <i class="fa-solid fa-file"></i>
-                <span class="link-text">User Visit Info</span>
+                <span class="link-text">Visit Records</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="#schedule-visit" class="nav-link">
                 <i class="fa-solid fa-calendar"></i>
-                <span class="link-text">Schedule a Visit</span>
+                <span class="link-text">Schedule Visit</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="#notifications" class="nav-link">
                 <i class="fa-solid fa-circle-exclamation"></i>
                 <span class="link-text">Notifications</span>
             </a>
--- pages/userDashboard.php
+++ pages/userDashboard.php
@@ -15,13 +15,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NEUST CLINIC</title>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/includes/dependencies.inc.php'  ?>
+    <link rel="stylesheet" href="../css/userDashboard.css">
 </head>
 
 <body>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/userNav.inc.php'); ?>
     <main><!--MAIN TAG REQUIRED FOR NAV SPACING-->
-    
+        <section id="dashboard">
+            <h1>1</h1>
+        </section>
+        <section id="user-information">
+            <h1>2</h1>
+        </section>
+        <section id="visit-records">
+            <h1>3</h1>
+        </section>
+        <section id="schedule-visit">
+            <h1>4</h1>
+        </section>
+        <section id="notifications">
+            <h1>5</h1>
+        </section>
     </main>
+
 </body>
 
 </html>
