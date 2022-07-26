6d09105b549d19dc70f4cc407f2bbd271bb1625a
Jemxxxx <105546355+Jemxxxx@users.noreply.github.com>
2022-07-26 20:42:53 +0800

    Added admin Dashboard/Navigation

--- pages/adminDashboard.php
+++ pages/adminDashboard.php
@@ -10,14 +10,60 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NEUST CLINIC</title>
+     <title>NEUST CLINIC</title>
+     <?php include $_SERVER['DOCUMENT_ROOT'].'/includes/dependencies.inc.php'  ?>
+     <link rel="stylesheet" href="../css/userDashboard.css">
+    <script defer src="../js/nav.js"></script>
+   
 </head>
 
 <body>
-    <h1>Admin Dashboard</h1>
-    <a href="/admin/logout">Logout</a>
+      <?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/userNav.inc.php'); ?>
+    <main>
+        <!--MAIN TAG REQUIRED FOR NAV SPACING-->
+        <section id="home">
+            <span>
+                <h1>Home</h1>
+            </span>
+            <div class="card bg-white shadow overflow-auto">
+                <p class="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, incidunt quisquam
+                    voluptatum quod itaque ab animi facere placeat. Voluptate quibusdam harum tempora numquam modi qui
+                    quos, repudiandae aperiam voluptatibus debitis?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempore, autem necessitatibus dolorem
+                    esse dolores quisquam non commodi quod tenetur saepe, perspiciatis nihil magnam! Nostrum id quae
+                    natus ea aspernatur.</p>
+            </div>
+        </section>
+        <section id="user-information">
+            <span>
+                <h1>User Information</h1>
+            </span>
+        </section>
+        <section id="visit-records">
+            <span>
+                <h1>Visit Records</h1>
+            </span>
+        </section>
+        <section id="schedule-visit">
+            <span>
+                <h1>Schedule Visit</h1>
+            </span>
+        </section>
+        <section id="notifications">
+            <span>
+                <h1>Notifications</h1>
+            </span>
+        </section>
+        <section id="settings">
+            <span>
+                <h1>Settings</h1>
+            </span>
+            
+            <a href="/admin/logout">logout</a>
+        </section>
 
 </body>
 
