b581202942b8a83ed832e45530a7fdd38e9e5b7e
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-07-28 02:39:59 +0800

    Made the refresh button responsive.

--- pages/admin/adminApproveRegistration.php
+++ pages/admin/adminApproveRegistration.php
@@ -26,7 +26,7 @@
         <!--MAIN TAG REQUIRED FOR NAV SPACING-->
         <div class="card shadow mb-3 cs-12 rs-1 p-2"><span class="my-auto d-flex justify-content-between"><h5 class="my-auto">Approve Users</h5><span class="d-flex flex-column"><h6><?php if(isset($_SESSION['admin_username']))  {echo $_SESSION['admin_username'];}  ?></h6><h6>Administrator</h6></span></span></div>
         <div class="card shadow bg-light cs-12 rs-7">
-            <span class="card-header d-flex justify-content-between"><h4>Registration</h4><button class="btn btn-primary text-light font-weight-bold" id="refresh"><i class="fa-solid fa-rotate-right mr-2"></i>Refresh</button></span>
+            <span class="card-header d-flex justify-content-between"><h4>Registration</h4><button class="btn btn-primary text-light font-weight-bold" id="refresh"><i class="fa-solid fa-rotate-right" id="refresh-icon"></i><span class="ml-2" id="refresh-text">Refresh</span></button></span>
             <div class="card-body" id="registration">
             </div>
         </div>
